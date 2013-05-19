cccd85b10bae2aba0a42961062ba3c219dfcace7
ErhanBasa <erhanbasa@Erhan-MacBook-Pro.local>
2013-05-19 14:14:53 +0300

    fixes

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,3 +1,10 @@
 document.getElementById('id_name').setAttribute("placeholder", "İsim Soyisim");
 document.getElementById('id_mail').setAttribute("placeholder", "E-Mail Adres");
 document.getElementById('id_message').setAttribute("placeholder", "Konu");
+
+document.createElement("header");
+document.createElement("footer");
+document.createElement("section"); 
+document.createElement("aside");
+document.createElement("nav");
+document.createElement("article"); 
